159f9c7a36cc4e7cb55eaf7aeb20cfa0bc9a24ed
Swastik Banerjee1 <swastban@in.ibm.com>
2023-12-13 17:57:49 +0530

    code checking 13-DEC-2023 1

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -3,6 +3,7 @@ package com.reactivespring.client;
 import com.reactivespring.config.WebClientConfig;
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.exception.MoviesInfoClientException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class MoviesInfoRestClient {
 
     private WebClient webClient;
@@ -29,6 +31,7 @@ public class MoviesInfoRestClient {
                 .uri(url, movieInfoId)
                 .retrieve()
                 .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                    log.error("Status code is: {}", clientResponse.statusCode().value());
                     if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
                         return Mono.error(
                                 new MoviesInfoClientException(
